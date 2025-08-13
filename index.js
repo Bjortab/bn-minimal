3893e6badc683f04c23e00e24d38caf1405290fe
Bjortab <bjorta.b@icloud.com>
2025-08-13 13:15:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// index.js
 export default {
-  async fetch(req) {
-    return new Response("Hello World från BN Minimal!", {
-      headers: { "content-type": "text/plain" },
+  async fetch(request, env, ctx) {
+    return new Response('Hello World från BN Minimal!', {
+      headers: { 'content-type': 'text/plain; charset=utf-8' },
     });
   },
 };
