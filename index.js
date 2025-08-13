14e76f78da80396bde33286f135bbb33be25d2cc
Bjortab <bjorta.b@icloud.com>
2025-08-13 13:57:26 +0200

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World från BN Minimal!', {
-      headers: { 'content-type': 'text/plain; charset=utf-8' },
-    });
-  },
-};
