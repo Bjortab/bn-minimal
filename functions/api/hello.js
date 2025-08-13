a4f1ff4bcd422c40abb41d70775d19b82a6d4a8a
Bjortab <bjorta.b@icloud.com>
2025-08-13 14:32:44 +0200

    Update hello.js

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -1,1 +1,7 @@
-
+export async function onRequest({ request }) {
+  const idea = await request.text();
+  const reply = idea
+    ? `Hej! Du skickade: “${idea}”. (Detta är bara ett demo-svar.)`
+    : 'Hej från /api/hello (POSTa en idé i body för demo-svar).';
+  return new Response(reply, { headers: { 'content-type': 'text/plain; charset=utf-8' }});
+}
